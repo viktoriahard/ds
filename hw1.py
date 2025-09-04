feba5edc0ae8e41b5068bcd49340953c73b44551
viktoria hardzei <viktoria_gordey@mail.ru>
2025-09-04 22:56:18 +0300

    1

--- hw1.py
+++ hw1.py
@@ -1,6 +1,6 @@
 data_tuple = (23, 77.7, -301, "Data", ["data", 100, 5, "hello"], {"name": "Viktoria"})
 print("Элементы:")
-for i in range(5):
+for i in range(6):
     a = data_tuple[i]
     if type(a) == int or type(a) == float:
         print(a)
