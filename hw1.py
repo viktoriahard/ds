d836a46f4dbd654c4a7498dcccc0b1299ff797bd
viktoria hardzei <viktoria_gordey@mail.ru>
2025-09-05 09:38:33 +0300

    1

--- hw1.py
+++ hw1.py
@@ -1,6 +1,8 @@
-data_tuple = (23, 77.7, "Data", ["data", 100, 5, "hello"], {"name": "Viktoria"}, -301)
-print("Элементы:")
-for i in range(6):
-    a = data_tuple[i]
-    if type(a) == int or type(a) == float:
-        print(a)
+data_tuple = (23, 77.7, "Data", ["data", 100, 5, "hello"], {"name": "Viktoria"}, -301)  # O(1)
+print("Элементы:")  # O(1)
+for i in range(len(data_tuple)):    # O(N) где N = 6 раз
+    a = data_tuple[i]   # O(1)
+    if type(a) == int or type(a) == float:  # O(1)
+        print(a)    # O(1)
+
+# Линейная сложность O(N)
